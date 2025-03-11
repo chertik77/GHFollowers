cd5a800a8669794b345c03de1e57279dc00cd227
chertik77 <denysbabych.dev@gmail.com>
2025-03-11 12:19:19 +0000

    feat: add FollowerListVc, and configure method to pass data to it from usernameTextField

---
+++ GHFollowers/FollowerListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FollowerListVC.swift
+//  GHFollowers
+//
+//  Created by Denys Babych on 11/03/2025.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
