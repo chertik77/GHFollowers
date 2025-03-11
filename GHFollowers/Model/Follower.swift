707b729e3e950a000cd6cbb7066a380f514a532d
chertik77 <denysbabych.dev@gmail.com>
2025-03-11 15:25:37 +0000

    feat: add user and follower models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Denys Babych on 11/03/2025.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
