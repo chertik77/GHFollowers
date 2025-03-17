b4ccc66cb751903f81515cb7ee28188c04ee919e
chertik77 <denysbabych.dev@gmail.com>
2025-03-17 22:44:19 +0000

    feat: add logic for searching follower in follower list vc

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -13,6 +13,7 @@ class FollowerListVC: UIViewController {
 
     var username: String!
     var followers: [Follower] = []
+    var filteredFollowers: [Follower] = []
     var page = 1
     var hasMoreFollowers = true
     
@@ -22,9 +23,10 @@ class FollowerListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureSearchController()
         configureCollectionView()
-        configureDataSource()
         getFollowers(username: username, page: page)
+        configureDataSource()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -67,6 +69,17 @@ class FollowerListVC: UIViewController {
         })
     }
     
+    func configureSearchController() {
+        let searchController = UISearchController()
+
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search for a username"
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
     func getFollowers(username: String, page: Int) {
         showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
@@ -86,7 +99,8 @@ class FollowerListVC: UIViewController {
                     }
                     return
                 }
-                self.updateData()
+            
+                self.updateData(on: followers)
         
             case.failure(let error):
                 self.presentGFAlertOnMainThread(
@@ -98,7 +112,7 @@ class FollowerListVC: UIViewController {
         }
     }
     
-    func updateData() {
+    func updateData(on followers: [Follower]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         
         snapshot.appendSections([.main])
@@ -110,7 +124,7 @@ class FollowerListVC: UIViewController {
 
 extension FollowerListVC: UICollectionViewDelegate {
 
-    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint) {
         let offsetY = scrollView.contentOffset.y
         let contentHight = scrollView.contentSize.height
         let height = scrollView.frame.size.height
@@ -120,5 +134,20 @@ extension FollowerListVC: UICollectionViewDelegate {
             page += 1
             getFollowers(username: username, page: page)
         }
+    }
+}
+
+extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else {
+            return
+        }
+        
+        filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
+        updateData(on: filteredFollowers)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        updateData(on: followers)
     }
 }
